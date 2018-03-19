722374e03a8aaab1d5d9bd198c1ea406af5a750b
Steven Roberts <contact@stevenroberts.info>
2018-03-19 11:27:12 +0000

    added command line functions to be recognised

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const   fs = require('fs'),
         _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+
+ if(command === 'add') {
+     console.log("Add a note");
+ } else if(command === 'list') {
+     console.log("List all notes");
+ } else {
+     console.log("Command not regonised");
+ }
