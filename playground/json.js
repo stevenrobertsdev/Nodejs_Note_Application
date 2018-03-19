561c275bc4746fc829dbcfb324214fe0e58c1f41
Steven Roberts <contact@stevenroberts.info>
2018-03-19 13:35:08 +0000

    added some JSON code to playground

--- playground/json.js
+++ playground/json.js
@@ -1,3 +1,32 @@
-var obj = {
-    name: "Steven Roberts"
+// var obj = {
+//     name: "Steven"
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Steven Roberts", "age": 24}';
+// var personObj = JSON.parse(personString);
+// console.log(typeof personObj);
+// console.log(personObj);
+
+const fs = require('fs');
+
+var originalNote = {
+    title:"Some title",
+    body: "Some body text"
 };
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync("note.json", originalNoteString, (err)=>{
+    if(err) throw err;
+})
+
+var noteString = fs.readFileSync('note.json',(err)=> {
+    if(err) throw err;
+})
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
