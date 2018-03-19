0eafeb35167b00d7f3f3595e528332542a121c80
Steven Roberts <contact@stevenroberts.info>
2018-03-19 13:53:05 +0000

    added a try catch method to JavaScript

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log("Adding note:" , title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch(err) {
+        
+    }
+
+    notes.push(note);
+
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes), (err) => {
+        if(err) throw err;
+    })
+
 };
 
 var getAll = () => {
