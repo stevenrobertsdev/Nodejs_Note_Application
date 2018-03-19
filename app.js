eb6cd08f7e2539e3a7b1b0bbb28cee27c3be269a
Steven Roberts <contact@stevenroberts.info>
2018-03-19 10:43:12 +0000

    used appendFile to create and populate a next text file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 console.log("Starting app.");
 
-const fs = require('fs');
+const   fs = require('fs'),
+        os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', user.username , (err) => {
+    if(err) throw err;
+});
+
